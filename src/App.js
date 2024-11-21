df09ea67b75d68f56c9ffe76b5cb770263051ea7
ganiajmeera <ganiajmeera74@gmail.com>
2024-11-21 21:53:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Link, Navigate, Route,Routes } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const USER_TYPES ={
+  PUBLIC:'public User',
+  NORMAL_USER:'Normal User',
+  ADMIN_USER:'Admin user'
 }
 
+const CURRENT_USER_TYPE = USER_TYPES.ADMIN_USER
+// const CURRENT_USER_TYPE = USER_TYPES.NORMAL_USER
+// const CURRENT_USER_TYPE = USER_TYPES.ADMIN_USER
+
+function App(){
+  return(
+    <div style={{display:"flex",gap:12,padding:8,backgroundColor:"skyblue"}}>
+      <Link to={'/'}>Home</Link>
+      {(CURRENT_USER_TYPE === USER_TYPES.NORMAL_USER || CURRENT_USER_TYPE === USER_TYPES.ADMIN_USER)?
+      <>
+       <Link to={'/user'}>User</Link>
+      <Link to={'/userProfile'}>User Profile</Link>
+      </>:null}
+      {( CURRENT_USER_TYPE === USER_TYPES.ADMIN_USER)?
+      <>
+     <Link to={'/admin'}>Admin</Link>
+      </>:null}
+      <br></br>
+      <div>
+        <AppRoutes/>
+        </div>
+    </div>
+  )
+}
+
+function AppRoutes(){
+  return(
+    <div>
+      <Routes>
+        <Route path='/' element={<PublicElement> <Home/></PublicElement>}></Route>
+        <Route path='/user' element={<UserElement><User/></UserElement>}></Route>
+        <Route path='/admin' element={<AdminElement><Admin/></AdminElement>}></Route>
+        <Route path='*' element={<div>Page is Not Found</div>}></Route>
+      </Routes>
+    </div>
+  )
+}
+
+function Home(){
+  return <div>Home</div>
+}
+function User(){
+  return <div>User Page</div>
+}
+function Admin(){
+  return <div>Admin Page</div>
+}
+
+function PublicElement({children}){
+  return (
+  <>
+  {children}
+  </>
+  )
+}
+function UserElement({children}){
+
+    if(CURRENT_USER_TYPE === USER_TYPES.NORMAL_USER || CURRENT_USER_TYPE === USER_TYPES.ADMIN_USER){
+      return (
+        <>
+        {children}
+        </>
+      )
+    }else{
+      return(<Navigate to={"/"}/>  
+      )
+    } 
+}
+function AdminElement({children}){
+
+  if( CURRENT_USER_TYPE === USER_TYPES.ADMIN_USER){
+    return (
+      <>
+      {children}
+      </>
+    )
+  }else{
+    return(<>You Dont Have To Access This Page</> 
+    )
+  }
+
+}
 export default App;
+
